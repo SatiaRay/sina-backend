62af826a5f499376e9ef6eb9271e42c037c7dae9
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-16 01:07:30 -0700

    feat(ai-knowledge): implement delete endpoint for document removal and enhance formatting in main.py

--- ai-knowledge/src/main.py
+++ ai-knowledge/src/main.py
@@ -18,16 +18,35 @@ def store(document: Document, response: Response):
         vector.add_documents([document])
 
         return {
-            "msg" : "succeed",
+            "msg": "succeed",
         }
     except Exception as e:
         print("Error in storing document:", e)
 
         response.status_code = 500
-        
-        return {"msg" : "Store document failed !"}
+
+        return {"msg": "Store document failed !"}
 
 
 @app.get('/search')
-def search(query:str):
+def search(query: str):
     return vector.search(query=query)
+
+
+class DeleteDocumentsRequest(BaseModel):
+    ids: list[str]
+
+@app.delete("/delete")
+def delete(request: DeleteDocumentsRequest, response: Response):
+    try:
+        vector.delete_documents(request.ids)
+
+        return {
+            "msg": "succeed",
+        }
+    except Exception as e:
+        print("Error in deleting document:", e)
+
+        response.status_code = 500
+        
+        return {"msg" : "Delete documents failed !"}
