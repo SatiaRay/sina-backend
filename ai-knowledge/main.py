79fdab87ff681b550a8b9f512a74373ac1505f9c
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-16 00:44:20 -0700

    feat(ai-knowledge): add endpoint to store documents in VectorStore

--- ai-knowledge/main.py
+++ ai-knowledge/main.py
@@ -1,10 +1,30 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from openai import BaseModel
+from models import Document
+from vector_store import VectorStore
 
 app = FastAPI()
+vector = VectorStore()
 
 
 @app.get("/test")
 def test():
     return {"msg": "The service is up !"}
+
+
+@app.post("/store")
+def store(document: Document, response: Response):
+    try:
+        vector.add_documents([document])
+
+        return {
+            "msg" : "succeed",
+        }
+    except Exception as e:
+        print("Error in storing document:", e)
+
+        response.status_code = 500
+        
+        return {"msg" : "Store document failed !"}
+
+
