c6521933978a5cc7117ea754b7fd04bd56ab2e7f
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-10-22 00:28:11 -0700

    test(database): update database connection logic to support testing environment with SQLite

--- knowledge/src/database.py
+++ knowledge/src/database.py
@@ -7,19 +7,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@"
-    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+IS_TESTING = os.environ.get('TESTING') == '1'
+
+if IS_TESTING:
+    SQLALCHEMY_DATABASE_URL = 'sqlite:///./test_temp.db'
+else:
+    SQLALCHEMY_DATABASE_URL = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@"
+        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-
-# Dependency for FastAPI
 def get_db():
     db = SessionLocal()
     try:
