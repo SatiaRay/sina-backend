7293d4efc7f50c3e755859df1d63a4a957b318b6
Mahdi Mahmoudi <dev@mahdi-mahmoudi.ir>
2025-12-13 22:40:38 -0800

    refactor: rename WorkspaceScope to WorkspaceGlobalScope

--- idp/app/Models/Workspace.php
+++ idp/app/Models/Workspace.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\HasWorkspaceScope;
+use App\Models\Scopes\HasWorkspaceGlobalScope;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
